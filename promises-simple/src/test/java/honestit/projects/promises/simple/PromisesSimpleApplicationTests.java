02956b0eebebd3352d7337a0b4b1737070621a04
honestit <michal.kupisinski@gmail.com>
2020-09-21 16:51:54 +0200

    Initial commit for promises-simple

---
+++ promises-simple/src/test/java/honestit/projects/promises/simple/PromisesSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package honestit.projects.promises.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromisesSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
