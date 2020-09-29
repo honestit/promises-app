8ce6606eab084cf38c0b32587aac0af430e5a956
delola\delola lokal <aleksandra.man@gmail.com>
2020-09-29 15:30:00 +0200

    Rewriter UserService::registerUser tests

--- promises-simple/src/test/java/honestit/projects/promises/simple/users/impl/DefaultUserServiceTest.java
+++ promises-simple/src/test/java/honestit/projects/promises/simple/users/impl/DefaultUserServiceTest.java
@@ -3,7 +3,16 @@ package honestit.projects.promises.simple.users.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import honestit.projects.promises.simple.users.domain.User;
+import honestit.projects.promises.simple.users.domain.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,33 +20,61 @@ import org.springframework.test.context.ActiveProfiles;
 import honestit.projects.promises.simple.users.UserRegisterRequest;
 import honestit.projects.promises.simple.users.UserRegisterResponse;
 
+import javax.swing.text.html.HTMLDocument;
 
-@SpringBootTest @ActiveProfiles("local")
+@DisplayName("User Service tests")
 class DefaultUserServiceTest {
-	
-	@Autowired
-	private DefaultUserService service;
 
-	@Test
-	void givenValidUserRegisterRequest_whenRegister_thenShouldBeOk() {
-		// given
-		UserRegisterRequest request = new UserRegisterRequest("test", "test", "test", "test", "test");
-		
-		// when
-		UserRegisterResponse response = service.register(request);
-		
-		// then
-		assertNotNull(response);
-		assertNotNull(response.getUserId());
-		assertEquals("test", response.getUsername());
-	}
-	
-	@Test 
-	public void givenUserRegisterRequestWithExistingUsername_whenRegister_thenThrowException() {
-		UserRegisterRequest request = new UserRegisterRequest("username1", "pass", "test", "test", "test2");
-		
-		service.register(request);
-		org.junit.jupiter.api.Assertions.assertThrows(IllegalStateException.class, () -> service.register(request));
-	}
+    private UserRepository userRepository;
+    private DefaultUserService service;
+
+    @BeforeEach
+    public void prepare() {
+        userRepository = Mockito.mock(UserRepository.class);
+        service = new DefaultUserService(userRepository);
+    }
+
+    @Nested
+    @DisplayName("User Service Tests - method registerUser tests")
+    class RegisterUserTest {
+
+        private final UserRegisterRequest request = new UserRegisterRequest("User", "User", "User", "Password", "User@User");
+
+        @Test
+        @DisplayName("When register user with valid data then user should be saved")
+        public void whenRegisterUserWithValidDataThenUserShouldBeSaved() {
+            ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+            Mockito.when(userRepository.save(ArgumentMatchers.any()))
+                    .thenAnswer(invocation -> {
+                        invocation.getArgument(0, User.class).setId(1L);
+                        return invocation.getArgument(0, User.class);
+                    });
+
+            UserRegisterResponse response = service.register(request);
+
+            Assertions.assertThat(response).isNotNull();
+            Assertions.assertThat(response.getUserId()).isNotNull().isEqualTo(1L);
+            Assertions.assertThat(response.getUsername()).isNotNull().isEqualTo("User");
+        }
+
+        @Test
+        @DisplayName("When register user with existing username then should throw exception")
+        public void whenRegisterUserWithExistingUsernameThenShouldThrowException() {
+            Mockito.when(userRepository.existsByUsername(ArgumentMatchers.any())).thenReturn(true);
+
+            Assertions.assertThatThrownBy(() -> service.register(request))
+                    .isInstanceOf(RuntimeException.class);
+        }
+
+        @Test
+        @DisplayName("When register user with existing email then should throw exception")
+        public void whenRegisterUserWithExistingEmailThenShouldThrowException() {
+            Mockito.when(userRepository.existsByEmail(ArgumentMatchers.any())).thenReturn(true);
+
+            Assertions.assertThatThrownBy(() -> service.register(request))
+                    .isInstanceOf(RuntimeException.class);
+        }
+
+    }
 
 }
