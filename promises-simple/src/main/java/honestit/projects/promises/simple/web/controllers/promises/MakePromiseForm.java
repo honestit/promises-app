d9ac2e19d6c952520d9f507aa66441988618617b
delola\delola lokal <aleksandra.man@gmail.com>
2020-10-01 14:47:30 +0200

    Make promise functionality implementation

---
+++ promises-simple/src/main/java/honestit/projects/promises/simple/web/controllers/promises/MakePromiseForm.java
@@ -0,0 +1,29 @@
+package honestit.projects.promises.simple.web.controllers.promises;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class MakePromiseForm {
+
+    @NotBlank
+    @Size(min = 3, max = 12)
+    private String friendName;
+    @NotBlank
+    private String title;
+    private String content;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate tillDate;
+    @NotNull
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime tillTime;
+}
